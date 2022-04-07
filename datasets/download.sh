f3fd6632419ec1c1017661bc456c40a765f9962d
karthikiitm87 <84072919+karthikiitm87@users.noreply.github.com>
2022-04-07 22:32:30 +0100

    Create download.sh
    This can be used to download the GWHD and Sim10K datasets.

---
+++ datasets/download.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wget http://droplab-files.engin.umich.edu/repro_10k_images.tgz
+unzip repro_10k_images.tgz
+wget http://droplab-files.engin.umich.edu/repro_10k_annotations.tgz
+unzip repro_10k_annotations.tgz
+wget https://zenodo.org/record/5092309/files/gwhd_2021.zip
+unzip gwhd_2021.zip
